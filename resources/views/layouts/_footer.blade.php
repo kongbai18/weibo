cba093b3b3afe6a7d2106b9b77cffe75024949a5
ligo <kongbai18@126.com>
2020-09-03 17:20:05 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44" alt="">
+  <a href="https://learnku.com/laravel" target="_blank">
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/aboot">关于</a>
+  </div>
+</footer>
